f18ed9e0c23622795f1e0eeb0f7e2b90af838c74
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-27 20:29:37 +0200

    3 task done

---
+++ 9_paskaita/3task.py
@@ -0,0 +1,21 @@
+def greeting(name1, name2, name3):
+    if not name1 or not name2 or not name3:
+        return
+    hello_string = f'Labas, {name1}!, Labas {name2}!, Labas {name3} '
+    return hello_string
+
+print(greeting('Bronius', 'Stasys', 'Petras'))
+
+
+
+print('------------------------------------------')
+
+
+def sveikink_su_pavadinimu(vardas, pavadinimas = 'drauge'):
+    return f'Sveikas {vardas}! Ka veiki, {pavadinimas}'
+
+print(sveikink_su_pavadinimu('Kazys'))
+# print(sveikink_su_pavadinimu('Kazys', 'kolega'))
+print(sveikink_su_pavadinimu(
+    vardas='Kazys',
+    pavadinimas= 'kolega'))
--- 9_paskaita/teorija.py
+++ 9_paskaita/teorija.py
@@ -12,19 +12,78 @@
 # for i in range(10):
 #     say_hello()
 
-name = 'Er'
+# name = 'Er'
+#
+# def create_greetings(user):
+#     if not user:
+#         return
+#     res = f'greetings, {user}'
+#     return res
+#
+# greetings = create_greetings(name)
+#
+# if greetings:
+#     print(greetings)
+# else:print('No user! oh no')
 
-def create_greetings(user):
-    if not user:
-        return
-    res = f'greetings, {user}'
-    return res
+#----------------------------
+#
+# def say_hello_to_two(name1, name2):
+#     if not name1 or not name2:
+#         return
+#     hello_string = f'hello to {name1} and Hello to {name2}'
+#     return hello_string
+#
+# res = say_hello_to_two('Pranas', 'Jonas')
+#
+# print(res)
 
-greetings = create_greetings(name)
+#----------------------------Numatyti argumentai:----------------
 
-if greetings:
-    print(greetings)
-else:print('No user! oh no')
+# def greet(name='Friend'):
+#     print(f'Hello {name}!')
+
+# greet('Erne')
+# greet()
+#
+# def priimk_pacienta(pacientas, gydytojas='gyd. Pagalnutylenis'):
+#     irasas_gyd_zurnale = f'Pacientas {pacientas}, prieme {gydytojas}'
+#     return irasas_gyd_zurnale
+#
+#
+# res = priimk_pacienta('Bronius')
+# print(res)
+#
+# res = priimk_pacienta('Varvara')
+# print(res)
+#
+# res = priimk_pacienta(
+#     'Staska',
+#     'gyd. Pakaitenis')
+# print(res)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
