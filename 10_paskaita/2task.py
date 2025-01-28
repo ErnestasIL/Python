af8c487a477c5251e8b3a005ed54923ef3d348b4
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-28 18:56:14 +0200

    2 task done

---
+++ 10_paskaita/2task.py
@@ -0,0 +1,12 @@
+from  random import randint, choice
+
+print(randint(1, 10))
+print('---------')
+print(choice(['banana', 'cherry', 'apple', 'pear']))
+print('---------')
+
+#---------------
+
+import datetime as dt
+
+print(f'Dabartine data ir laikas {dt.datetime.now()}')
--- 10_paskaita/teorija.py
+++ 10_paskaita/teorija.py
@@ -1,13 +1,51 @@
 #--------- moduliai-----------
 
-import random
+# import random
+#
+# atsitiktinis_skaicius = random.randint(1, 10)
+#
+# print(f'Atsitiktinis int skaicius nuo 1 iki 10: {atsitiktinis_skaicius}')
+# print(f'Atsitiktinis int skaicius nuo 1 iki 10: {random.randint(1, 10)}')
+#
+# while True:
+#     skaicius = int(input('Iveskite skaiciu, kol neatpesite tinkamo neiseisite:'))
+#     if skaicius == random.randint(1, 10):
+#         break
 
-atsitiktinis_skaicius = random.randint(1, 10)
 
-print(f'Atsitiktinis int skaicius nuo 1 iki 10: {atsitiktinis_skaicius}')
-print(f'Atsitiktinis int skaicius nuo 1 iki 10: {random.randint(1, 10)}')
+#---------- random importavimas
+#
+# from random import randint, choice
+#
+# random_number = randint(1, 10)
+# print(random_number)
+#
+# random_month = choice(['sausis', 'varasis', 'kovas'])
+#
+# print(random_month)
 
-while True:
-    skaicius = int(input('Iveskite skaiciu, kol neatpesite tinkamo neiseisite:'))
-    if skaicius == random.randint(1, 10):
-        break
+
+#--------- modulio trumpinimas
+
+import random as rn
+
+random_number = rn.randint(1, 25)
+
+random_month = rn.choice(['sausis', 'varasis', 'kovas'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
