90d811bd96aac12f0b6238058ade2a581ae847c1
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-29 21:17:07 +0200

    task 2 and 3, done

--- 11_paskaita/3task.py
+++ 11_paskaita/3task.py
@@ -3,12 +3,15 @@ def rodyti_duomenis(**kwargs):
         print(f'{raktas}: {reiksme}')
 rodyti_duomenis(vardas="Petras", pavarde="Petraitis", miestas="Prienai")
 
+print('---------------------')
+
 def registruoti_naudotoja(vardas, el_pastas, **kwargs):
     print(f"Vardas: {vardas}\nEl. paštas: {el_pastas}")
     for raktas, reiksme in kwargs.items():
         print(f"{raktas}: {reiksme}")
 
 registruoti_naudotoja('John', 'john.cena@cantseeme.com', ocupation='wrestler', lytis='male')
+print('---------------------')
 
 random_list = ['something', 'creeping', 'behind', 'you']
 def atspausdinti_lista(listas, **kwargs):
