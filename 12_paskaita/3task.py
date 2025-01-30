1962a3ee2ae444981d8e59fa5f57df8b10a303da
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-30 20:44:24 +0200

    task 3, done (pasirodo veikia while true< tik reikejo perkelt 'ivestis'

--- 12_paskaita/3task.py
+++ 12_paskaita/3task.py
@@ -1,10 +1,14 @@
-ivestis = input('Provide a number: ')
-try:
-    res = int(ivestis)
-    print(res)
-except:
-    print('huh?')
-else:
-    print(f'conversion succesfull: {res}')
-finally:
-    print('Program closed...')
+
+while True:
+    ivestis = input('Provide a number: ')
+    try:
+        res = int(ivestis)
+        print(res)
+    except:
+        print('huh?')
+    else:
+        print(f'conversion succesfull: {res}')
+    finally:
+        print('Program closed...')
+    print('-------------------')
+
