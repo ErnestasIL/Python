8470142d39d045f5430594abef635ac4481a89cc
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-02-03 21:54:21 +0200

    task 3, done

--- 13_paskaita/teorija.py
+++ 13_paskaita/teorija.py
@@ -23,27 +23,56 @@ import datetime
 # ivestis = '2020-02-11'
 # my_datetime = datetime.datetime.strptime(ivestis, '%Y-%m-%d')
 # print(my_datetime)
-months = {
-    1: 'sausis',
-    2: 'vasaris'
-}
+# months = {
+#     1: 'sausis',
+#     2: 'vasaris'
+# }
 
-ivestis = '2020-02-15, 10:11:59'
-
-my_datetime = datetime.datetime.strptime(ivestis, '%Y-%m-%d, %H:%M:%S')
+# ivestis = '2020-02-15, 10:11:59'
+#
+# my_datetime = datetime.datetime.strptime(ivestis, '%Y-%m-%d, %H:%M:%S')
 # print(my_datetime)
 
-print(months.get(my_datetime.month))
+# print(months.get(my_datetime.month))
+#
+# print(my_datetime.strftime('%d-%m-%Y'))
+# print(my_datetime.strftime('%d %B %Y'))
 
-print(my_datetime.strftime('%d-%m-%Y'))
-print(my_datetime.strftime('%d %B %Y'))
+#------------------------
+import datetime
+
+# dabar = datetime.datetime.today()
+# mileniumas = datetime.datetime(2000, 1, 1)
+
+# skirtumas = dabar - mileniumas
+# print(skirtumas)
+# print(type(skirtumas))
+
+# skirtumas = datetime.timedelta(hours=1000)
+# res =  dabar + skirtumas
+# print(res)
+#
+# skirtumas = datetime.timedelta(days=1000, hours=1000, minutes=100)
+# print(skirtumas)
+# print(skirtumas.days / 365)
+# print(skirtumas.seconds)
+# print(skirtumas.seconds / 60 / 60)
+# sekundes = skirtumas.total_seconds()
+# valanods = sekundes / 60 / 60
+# print(valanods)
+# dienos = valanods / 24
+# print(dienos)
+# metai = dienos / 365
+# print(metai)
 
 
 
 
 
 
-
+# res = dabar - skirtumas
+# print(res)
+#
 
 
 
