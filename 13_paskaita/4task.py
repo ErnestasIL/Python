0cb4a1a1e21a2c6258b84b69f72aa3cfa0a92ee2
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-02-04 18:41:26 +0200

    task 4, done

--- 13_paskaita/4task.py
+++ 13_paskaita/4task.py
@@ -1,25 +1,25 @@
 #1task
-import datetime
+from _datetime import datetime,timedelta
 
-first_date = datetime.datetime(2023, 1, 1)
-second_date = datetime.datetime(2024, 1, 1)
+first_date = datetime(2023, 1, 1)
+second_date = datetime(2024, 1, 1)
 date_res = second_date - first_date
-print(date_res)
+print(f'skirtumas: {date_res.days}')
 
 #2task
 
-current_date = datetime.datetime.now()
-future_date = datetime.timedelta(days=90)
+current_date = datetime.now()
+future_date = timedelta(days=90)
 res = current_date + future_date
-print(f'Data po 90 dienu bus: {res}')
+print(f"Data po 90 dienu bus: {res.strftime('%Y-%m-%d')}")
 
 #3task
 
-millennium_date = datetime.datetime(2000, 1, 1)
+millennium_date = datetime(2000, 1, 1)
 
 res2 = current_date - millennium_date
 
 print(f'days: {res2.days}d')
-print(f'hours(using seconds): {round(res2.seconds / 3600)}h')
-print(f'hours(using total_seconds): {round(res2.total_seconds() / 3600)}h')
-print(f'total seconds: {round(res2.total_seconds())}s')
+print(f'hours(using seconds): {int(res2.seconds // 3600)}h')
+print(f'hours(using total_seconds): {int(res2.total_seconds() / 3600)}h')
+print(f'total seconds: {int(res2.total_seconds())}s')
